543b5dba218b02ce10d9411cf25dfbb7431c9741
Davide Lanfranchi <davidelanfranchi@users.noreply.github.com>
2025-02-23 17:23:43 +0000

    Add new sketch

---
+++ src/members/davide/assets/sketch-00-2/scene/gui.js
@@ -0,0 +1,13 @@
+// import GUI from "lil-gui";
+// const gui = new GUI();
+// export default gui;
+
+// export function getController(propName) {
+//   for (const folder of gui.folders) {
+//     const controller = folder.controllers.find(
+//       (ctrl) => ctrl.property === propName
+//     );
+//     if (controller) return controller;
+//   }
+//   return gui.controllers.find((ctrl) => ctrl.property === propName);
+// }
---
+++ src/members/davide/assets/sketch-00-2/scene/settings.js
@@ -0,0 +1,6 @@
+export default {
+  parentElement: document.getElementById("app"),
+  backgroundColor: "#dcdcdc",
+  color1: "#add8e6",
+  color2: "#f08080",
+};
